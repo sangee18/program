fbaba0b48c37a9d1cd3a1289a272361536996128
sangee18 <52812563+sangee18@users.noreply.github.com>
2019-08-08 23:31:37 +0530

    Update great.cpp

--- great.cpp
+++ great.cpp
@@ -2,25 +2,17 @@
 using namespace std;
 int main()
 {
- int a,b,c;
+ int arr[40],i,j,n;
 int max=0;
-cin>>a>>b>>c;
-if(a>=b)
-  { if(a>=c)
-      max=a;
-    else
-      max=c;
-  }
-  else
-  if(b>a)
-  {
-      if(b>c)
-      max=b;
-      else
-      max=c;
-  }
-  else
-   max=c;
- cout<<max;
+cin>>n;
+for(int i=0;i<n;i++)
+   cin>>arr[i];
+for(int i=1;i<n;i++)
+{
+       if(arr[0]<arr[i])
+         arr[0]=arr[i];
+    
+}
+cout<<arr[0];
 return 0;
 }
